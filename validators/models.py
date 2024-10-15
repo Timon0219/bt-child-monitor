66e328882a22ffebc4bf2e75cad81a9d13b4ed60
Timon0219 <joinercantillo0813@gmail.com>
2024-10-14 23:18:22 -0700

    add ValidatorChildKeyinfo Django class

--- validators/models.py
+++ validators/models.py
@@ -1,58 +1,4 @@
-# from django.db import models
 
-# class Validator(models.Model):
-#     coldkey = models.CharField(max_length=255)
-#     hotkey = models.CharField(max_length=255, unique=True)
-#     stake = models.FloatField()
-#     parentkey_netuids = models.JSONField(default=list)
-#     childkeys = models.JSONField(default=list)
-#     parentkeys = models.JSONField(default=list)
-
-#     def __str__(self):
-#         return self.hotkey
-
-# from django.db import models
-# from django.contrib.postgres.fields import ArrayField
-
-# class Validators(models.Model):
-#     coldkey = models.CharField(max_length=255)
-#     hotkey = models.CharField(max_length=255, unique=True)
-#     stake = models.FloatField()
-#     parentkey_netuids = ArrayField(models.IntegerField(), default=list, blank=True)
-#     childkeys = models.JSONField(default=list, blank=True)
-#     parentkeys = models.JSONField(default=list, blank=True)
-
-#     def __eq__(self, other):
-#         if isinstance(other, Validators):
-#             return (self.coldkey == other.coldkey and
-#                     self.hotkey == other.hotkey)
-#         return False
-
-#     def __hash__(self):
-#         return hash((self.coldkey, self.hotkey))
-    
-#     def add_parentkeys(self, parent_hotkey, proportion, net_uid):
-#         # Method to add a parentkey dictionary to the parentkeys list
-#         parentkey_info = {
-#             'parent_key': parent_hotkey,
-#             'proportion': proportion,
-#             'net_uid': net_uid
-#         }
-#         self.parentkeys.append(parentkey_info)
-#         self.save(update_fields=['parentkeys'])
-        
-#     def add_childkeys(self, child_hotkey, proportion, net_uid):
-#         # Method to add a childkey dictionary to the childkeys list
-#         childkey_info = {
-#             'child_hotkey': child_hotkey,
-#             'proportion': proportion,
-#             'net_uid': net_uid
-#         }
-#         self.childkeys.append(childkey_info)
-#         self.save(update_fields=['childkeys'])
-
-#     def __str__(self):
-#         return self.hotkey
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 import json
@@ -106,3 +52,17 @@ class Validators(models.Model):
 
     def __str__(self):
         return self.hotkey
+
+
+
+class ValidatorChildKeyInfo(models.Model):
+    parent_hotkey = models.ForeignKey(Validators, to_field='hotkey', on_delete=models.CASCADE, related_name='child_keys', db_column='parent_hotkey')
+    parent_coldkey = models.CharField(max_length=255)
+    parent_stake = models.FloatField()
+    child_hotkey = models.CharField(max_length=255)
+    # child_stake = models.FloatField()
+    stake_proportion = models.FloatField()
+    subnet_uid = models.IntegerField()
+
+    class Meta:
+        unique_together = ('parent_hotkey', 'child_hotkey', 'subnet_uid')
